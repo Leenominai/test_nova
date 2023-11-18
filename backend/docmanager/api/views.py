09ce7123888435346102d4c922b30066a190ab87
Александр Рассыхаев <farexzon@yandex.ru>
2023-11-18 07:52:13 +0300

    Develop v1.20

--- backend/docmanager/api/views.py
+++ backend/docmanager/api/views.py
@@ -1,13 +1,14 @@
 import io
-import json
 import logging
 import os
 
 from django.views.decorators.csrf import csrf_exempt
 from drf_spectacular.utils import extend_schema
-from google.oauth2 import service_account
+from google.auth.transport.requests import Request
 from google.api_core.exceptions import NotFound
-from googleapiclient import discovery
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseUpload
 from rest_framework import status
 from rest_framework.decorators import api_view
@@ -15,15 +16,9 @@ from rest_framework.response import Response
 
 from .decorators import user_me_view_request_schema
 
-logger = logging.getLogger(__name__)
 
-
-def load_credentials():
-    credentials_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'credentials.json')
-    with open(credentials_path) as f:
-        credentials_data = json.load(f)
-
-    return service_account.Credentials.from_service_account_info(credentials_data)
+SCOPES = ["https://www.googleapis.com/auth/drive.file"]
+logging.basicConfig(level=logging.INFO)
 
 
 @extend_schema(tags=["Загрузка файла в Google Drive"])
@@ -50,13 +45,40 @@ def create_google_drive_document(request):
                 status=status.HTTP_400_BAD_REQUEST
             )
 
-        credentials = load_credentials()
-        user = request.user
-        print(user.id)
+        creds = None
+        token_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'token.json')
 
-        drive = discovery.build('drive', 'v3', credentials=credentials)
+        if os.path.exists(token_path):
+            creds = Credentials.from_authorized_user_file(token_path, SCOPES)
+            if creds.expired:
+                creds.refresh(Request())
+
+                with open(token_path, 'w') as token:
+                    token.write(creds.to_json())
+        else:
+            credentials_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'credentials.json')
+            flow = InstalledAppFlow.from_client_secrets_file(credentials_path, SCOPES)
+            creds = flow.run_local_server(port=0)
+
+            with open(token_path, 'w') as token:
+                token.write(creds.to_json())
 
         try:
+            drive = build("drive", "v3", credentials=creds)
+
+            existing_files = (
+                drive.files()
+                .list(q=f"name='{name}'", pageSize=1, fields="files(id, name)")
+                .execute()
+            )
+            if existing_files.get("files"):
+                # Файл с таким именем уже существует
+                logging.warning(f"Документ с именем {name} уже существует.")
+                return Response(
+                    {"message": "Документ с таким именем уже существует."},
+                    status=status.HTTP_200_OK
+                )
+
             file = drive.files().create(
                 body={"name": name, "mimeType": "text/plain"},
             ).execute()
@@ -64,7 +86,7 @@ def create_google_drive_document(request):
             media_body = MediaIoBaseUpload(io.BytesIO(data.encode("utf-8")), mimetype="text/plain", resumable=True)
             drive.files().update(fileId=file["id"], media_body=media_body).execute()
 
-            logger.info(f"Документ успешно создан с именем: {name}")
+            logging.info(f"Документ успешно создан с именем: {name}")
 
             file_id = file["id"]
             file_info = drive.files().get(fileId=file_id).execute()
@@ -78,21 +100,21 @@ def create_google_drive_document(request):
                 status=status.HTTP_200_OK
             )
         except NotFound:
-            logger.warning(f"Документ с именем {name} уже существует.")
+            logging.warning(f"Документ с именем {name} уже существует.")
 
             return Response(
                 {"message": "Документ с таким именем уже существует."},
                 status=status.HTTP_200_OK
             )
         except Exception as e:
-            logger.exception("Произошла ошибка при создании документа.")
+            logging.exception("Произошла ошибка при создании документа.")
 
             return Response({
                 "error": f"Произошла ошибка при создании документа: {e}"},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
 
-    logger.warning("Неверный запрос.")
+    logging.warning("Неверный запрос.")
 
     return Response(
         {"error": "Неверный запрос."},
